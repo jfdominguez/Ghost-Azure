07fa3c50dff1bf1c5c52b34ff6d7e75d51c1d77d
jfdominguez <jfdominguez@maarket.co>
2017-02-01 11:25:53 -0500

    Delete deploy.cmd

--- deploy.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ---Deploying site
-
-REM ---Deploy the wwwroot folder in repository to default target (wwwroot)
-xcopy %DEPLOYMENT_SOURCE%\wwwroot\* %DEPLOYMENT_TARGET% /Y /s 
-
-REM ---Deploy the myapp folder in repository to folder above default target (wwwroot\blog)
-xcopy %DEPLOYMENT_SOURCE%\* %DEPLOYMENT_TARGET%\blog /Y /s 
