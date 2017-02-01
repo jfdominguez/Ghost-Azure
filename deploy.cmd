47a1e71b34d81494fc53fb701f9b0fccb9417e4a
jfdominguez <jfdominguez@maarket.co>
2017-02-01 11:02:28 -0500

    Create deploy.cmd

---
+++ deploy.cmd
@@ -0,0 +1,8 @@
+@echo off
+echo ---Deploying site
+
+REM ---Deploy the wwwroot folder in repository to default target (wwwroot)
+xcopy %DEPLOYMENT_SOURCE%\wwwroot\* %DEPLOYMENT_TARGET% /Y /s 
+
+REM ---Deploy the myapp folder in repository to folder above default target (wwwroot\blog)
+xcopy %DEPLOYMENT_SOURCE%\blog\* %DEPLOYMENT_TARGET%\blog /Y /s 
